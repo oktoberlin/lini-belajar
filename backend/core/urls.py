0e65b715ad6e37046677fa3030bb67146e8dbd33
oktoberlin <oktoberlin@gmail.com>
2020-12-30 09:58:22 +0700

    'add-token-authentication'

---
+++ backend/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from apps.accounts.urls import accounts_urlpatterns
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += accounts_urlpatterns
